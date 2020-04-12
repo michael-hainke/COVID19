c160c51fa7f401ea4fa23074691a98e1e0738476
Michael Hainke <hainke@hotmail.com>
2020-04-12 10:05:02 -0700

    Add population data

--- Covid19.R
+++ Covid19.R
@@ -1,3 +1,4 @@
+# Packages
 library(plyr)
 library(tidyverse)
 
@@ -41,17 +42,42 @@ rm(df1)
 rm(df2)
 
 # Fix Date
-test$Last_Update <- gsub(" .+", "",test$Last_Update)
+df$Last_Update <- gsub(" .+", "",df$Last_Update)
 
-for (i in 1:length(test$Last_Update)) {
-  if (grepl("^\\d+/\\d+/\\d+", test$Last_Update[i])) {
-    month <- sprintf("%02d", as.numeric(gsub("/\\d+/\\d+$", "",test$Last_Update[i])))
-    day <- sprintf("%02d", as.numeric(gsub("^\\d+/|/\\d+$", "",test$Last_Update[i])))
-    year <- as.numeric(gsub("^\\d+/\\d+/", "",test$Last_Update[i]))
+for (i in 1:length(df$Last_Update)) {
+  if (grepl("^\\d+/\\d+/\\d+", df$Last_Update[i])) {
+    month <- sprintf("%02d", as.numeric(gsub("/\\d+/\\d+$", "",df$Last_Update[i])))
+    day <- sprintf("%02d", as.numeric(gsub("^\\d+/|/\\d+$", "",df$Last_Update[i])))
+    year <- as.numeric(gsub("^\\d+/\\d+/", "",df$Last_Update[i]))
     year <- if(year<2000) { year + 2000 } else { year }
-    test$Last_Update[i] <- paste0(year,"-",month,"-",day)
+    df$Last_Update[i] <- paste0(year,"-",month,"-",day)
   }
 }
 
-test$Last_Update <- as.Date(test$Last_Update)
+df$Last_Update <- as.Date(df$Last_Update)
+
+# Join Population Data
+pop <- read.csv('population.csv') %>%
+       select(-n)
+df <- left_join(df, pop)
+
+# Fix some Country Data
+df <- mutate(df, Country_Region = case_when(Country_Region == 'Korea, South' ~ 'South Korea',
+                                            Country_Region == 'United Kingdom' ~ 'UK',
+                                            Country_Region %in% c('Hong Kong','Hong Kong SAR', 'Mainland China') ~ 'China',
+                                            Country_Region == 'Iran (Islamic Republic of)' ~ 'Iran',
+                                            TRUE ~ Country_Region))
+df$Province_State[df$Province_State %in% c('UK','United Kingdom')] <- NA
+
+# Add Population Adjusted Data
+df <- mutate(df, Confirmed_per_mil = Confirmed / (Population/1000000),
+                 Deaths_per_mil = Deaths / (Population/1000000),
+                 Recovered_per_mil = Recovered / (Population/1000000),
+                 Active_per_mil = Recovered / (Population/1000000))
+
+
+
+# Test
+test <- filter(df, Province_State == "British Columbia")
+
 
